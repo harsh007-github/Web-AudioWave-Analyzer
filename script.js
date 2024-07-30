6dd518c2a329cdb32e8ccde27cc1bdbe4191441d
harsh007-github <khandelwalharsh401@gmail.com>
2024-07-30 14:19:59 +0530

    Audio Upload and play feature added

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+//Audio Effect or changes or modifications
 const input = document.querySelector("input");
-input.addEventListener("change", (e) => {
-    const inputFile = input.files;
-    console.log(inputFile); 
+const audioElem = document.querySelector("audio");
+//If facing any errors put "change" instead of input here in the event listener
+input.addEventListener("input", (e) => {
+    var inputFile = input.files[0];
+    if(!inputFile)
+        return;
+    //Here input file is an array
+    // console.log(inputFile);
+    audioElem.src = URL.createObjectURL(inputFile);
+    audioElem.play();
 });
