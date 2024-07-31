dcf9ad19bef647df39ae6351d54c974747868117
harsh007-github <khandelwalharsh401@gmail.com>
2024-07-31 22:43:03 +0530

    Audio context created

--- script.js
+++ script.js
@@ -3,11 +3,23 @@ const input = document.querySelector("input");
 const audioElem = document.querySelector("audio");
 //If facing any errors put "change" instead of input here in the event listener
 input.addEventListener("input", (e) => {
-    var inputFile = input.files[0];
-    if(!inputFile)
-        return;
-    //Here input file is an array
-    // console.log(inputFile);
-    audioElem.src = URL.createObjectURL(inputFile);
-    audioElem.play();
+  var inputFile = input.files[0];
+  if (!inputFile) return;
+  //Here input file is an array
+  // console.log(inputFile);
+  audioElem.src = URL.createObjectURL(inputFile);
+  // audioElem.play();
+  //Create audio context where all the work on audio will be done
+  const audioContext = new AudioContext();
+  //source -> analyser -> destination
+
+  //audio source node
+  const audioSource = audioContext.createMediaElementSource(audioElem);
+
+  //Analyser
+  const analyser = audioContext.createAnalyser();
+  audioSource.connect(analyser);
+
+  //Destination node
+  analyser.connect(audioContext.destination); // Speaker
 });
